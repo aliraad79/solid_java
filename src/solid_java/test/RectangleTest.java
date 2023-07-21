5cb1832d459b62717c0a58623c2432807d04daf7
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-07-21 23:14:50 +0330

    Add length and width change tests

--- src/solid_java/test/RectangleTest.java
+++ src/solid_java/test/RectangleTest.java
@@ -18,4 +18,32 @@ public class RectangleTest {
         Rectangle rectangle = new Rectangle(3,4);
         assert rectangle.computeArea() == 12;
     }
+
+    @Test
+    void rectangleSetLength(){
+        Rectangle rectangle = new Rectangle(3,4);
+        rectangle.setLength(5);
+
+        assert  rectangle.length == 5;
+    }
+
+    @Test
+    void rectangleGetLength(){
+        Rectangle rectangle = new Rectangle(3,4);
+        assert  rectangle.getLength() == 4;
+    }
+
+    @Test
+    void rectangleSetWidth(){
+        Rectangle rectangle = new Rectangle(3,4);
+        rectangle.setWidth(5);
+
+        assert  rectangle.width == 5;
+    }
+
+    @Test
+    void rectangleGetWidth(){
+        Rectangle rectangle = new Rectangle(3,4);
+        assert  rectangle.getWidth() == 3;
+    }
 }
