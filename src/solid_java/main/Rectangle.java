a78c929ce59021f861767424eca753d3291a57d7
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-07-21 23:15:01 +0330

    Add change width and length methods

--- src/solid_java/main/Rectangle.java
+++ src/solid_java/main/Rectangle.java
@@ -9,6 +9,21 @@ public class Rectangle {
         this.length = length;
     }
 
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 
     public int  computeArea() {
         return width * length;
