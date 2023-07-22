379f15ef0a452355bc732ad6f78b3161a8b67b22
Ali Ahmadi <kaf.ahmadi@tapsi.cab>
2023-07-22 09:40:23 +0330

    feat: Add square tests

--- src/solid_java/test/SquareTest.java
+++ src/solid_java/test/SquareTest.java
@@ -1,13 +1,34 @@
 package solid_java.test;
 
 import org.junit.jupiter.api.Test;
-import solid_java.main.Rectangle;
 import solid_java.main.Square;
 
 public class SquareTest {
     @Test
-    void createSquare(){
+    void createSquare() {
         Square square = new Square(3);
         assert square != null;
+    }
+
+    @Test
+    void squareComputeArea() {
+        Square square = new Square(3);
+        assert square.computeArea() == 9;
+    }
+
+
+    @Test
+    void squareGetLength() {
+        Square square = new Square(3);
+        assert square.getLength() == 3;
+    }
+
+    @Test
+    void squareSetLength() {
+        Square square = new Square(3);
+        square.setLength(5);
+
+        assert square.getLength() == 5;
+        assert square.computeArea() == 25;
     }
 }
