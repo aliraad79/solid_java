7db9510f16bd414ede67d99843cf1d2227820015
Ali Javanmard <ali.javanmard14@gmail.com>
2023-07-29 11:27:38 +0330

    Update Square.java

--- src/solid_java/main/Square.java
+++ src/solid_java/main/Square.java
@@ -2,16 +2,17 @@ package solid_java.main;
 
 public class Square implements Shape {
     private int length;
-
+    // Constructor of Square class, initializes the length variable
     public Square(int length) {
         this.length = length;
-    }
-
+    } 
+    // Overrides the computed Area method of the Shape interface
+    // Calculates and returns the area of the square
     @Override
     public int computeArea() {
         return length * length;
     }
-
+    // Setter and Getter method for the length variable
     public void setLength(int length) {
         this.length = length;
     }
